e34bde7b12a572e0d8684b62c103437f3fdb9b57
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-29 21:18:12 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -3,9 +3,15 @@ require 'pry'
 def my_all?(collection)
   block_return_values = Array.new
   i = 0
-  while i < collection.size 
+  while i < collection.size
     block_return_values.push(yield(collection[i]))
     i += 1
+  end
+
+  if block_return_values.include?(false)
+    return false
+  else
+    return true
   end 
 end
 
