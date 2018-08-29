6496508b76f1c4dc1db4438fe9e70d4902951511
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-08-29 21:17:04 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  block_return_values = Array.new
+  i = 0
+  while i < collection.size 
+    block_return_values.push(yield(collection[i]))
+    i += 1
+  end 
 end
+
+
+my_all?([1,2,3]) {|i| i < 2}
